4965f395e26af82f43c448ee8e65765aeb23c86e
Hadassah Harland <hadassah.harland@my.jcu.edu.au>
2016-10-23 11:08:42 +1000

    Retrieve and set playernames

--- src/SuperTrumpGUI.java
+++ src/SuperTrumpGUI.java
@@ -10,8 +10,11 @@ import java.awt.event.ActionListener;
 public class SuperTrumpGUI extends JFrame implements ActionListener {
     private static SuperTrumpGUI window;
     private static JButton start, quit, instruct, returnToMenu, nextImage, playersContinue, startGame, backToNoSelect;
-    private static JLabel instructImage;
+    private static JButton tempMenu, pass, viewHand;
+    private static JLabel instructImage, player2window, player3window, player4window, player5window;
     private static JComboBox<Integer> noPlayersChoice;
+    private static JTextField player1Name, player2Name, player3Name, player4Name, player5Name;
+    private String[] playerNames;
     private int instructImageIndex;
     private static final Dimension DIM = new Dimension(900,640);
     private static final Font HEADING = new Font("Century",Font.BOLD, 22);
@@ -20,6 +23,7 @@ public class SuperTrumpGUI extends JFrame implements ActionListener {
     private Container con = getContentPane();
     // TODO better way to do Instruct Images
     private String[] instructImageDetail = {"Slide61.jpg","Slide62.jpg","Slide63.jpg","Slide64.jpg"};
+    public Game newGame;
 
     public static void main(String[] args) {
         window = new SuperTrumpGUI();
@@ -80,16 +84,48 @@ public class SuperTrumpGUI extends JFrame implements ActionListener {
         if (source == playersContinue)  {
             RetrievePlayerNames();
         }
+        if (source == backToNoSelect)  {
+            RetrieveNoPlayers();
+        }
+        if (source == startGame)  {
+            SavePlayerNames();
+            GameGUI(playerNames);
+        }
+        if (source == tempMenu)  {
+            TempMenu();
+        }
+        if (source == pass)  {
+            // TODO pass
+        }
+        if (source == viewHand)  {
+            ViewHand();
+        }
     }
 
     public void GameGUI(String[] players) {
         con.removeAll();
-
         con.setLayout(new GridLayout(3, 4, 5, 5));
-        for (int i = 0; i < players.length-1; i++) {
-            JLabel player = new JLabel("Player" + (i + 1) + " has 3 cards");
-            con.add(player);
+
+        newGame = new Game(players.length);
+
+        player2window = new JLabel(players[1] + " has " + "playerHand.length" + " cards");
+        player2window.setFont(LABELS);
+        con.add(player2window);
+        player3window = new JLabel(players[2] + " has " + "playerHand.length" + " cards");
+        player3window.setFont(LABELS);
+        con.add(player3window);
+        if (players.length > 3)  {
+            player4window = new JLabel(players[3] + " has " + "playerHand.length" + " cards");
+            player4window.setFont(LABELS);
+            con.add(player4window);
+            if (players.length > 4)  {
+                player5window = new JLabel(players[4] + " has " + "playerHand.length" + " cards");
+                player5window.setFont(LABELS);
+                con.add(player5window);
+            }
         }
+
+        // add appropriate number of blank labels to fill the grid
         for (int i = players.length-1; i < 6; i++) {
             con.add(new JLabel());
         }
@@ -97,6 +133,13 @@ public class SuperTrumpGUI extends JFrame implements ActionListener {
         con.add(new JLabel("Dialogue Box"));
         con.add(new JButton("View Hand"));
         con.add(new JButton("Pass"));
+        con.add(new JLabel());                  // blank Label
+
+        // Temp Menu Button
+        tempMenu = new JButton("Menu");
+        tempMenu.setFont(BUTTONS);
+        con.add(tempMenu);
+
         invalidate();
         validate();
         repaint();
@@ -127,7 +170,7 @@ public class SuperTrumpGUI extends JFrame implements ActionListener {
         repaint();
     }
 
-    public void ViewInstructions()  {
+    private void ViewInstructions()  {
         con.removeAll();
         window.setResizable(false);
         instructImageIndex = 0;
@@ -155,7 +198,7 @@ public class SuperTrumpGUI extends JFrame implements ActionListener {
         repaint();
     }
 
-    public void RefreshInstructions()  {
+    private void RefreshInstructions()  {
         instructImageIndex++;
         if (instructImageIndex >= instructImageDetail.length)  {
             // If this is the last image, return to Menu
@@ -172,42 +215,37 @@ public class SuperTrumpGUI extends JFrame implements ActionListener {
         }
     }
 
-    public ImageIcon Scale(ImageIcon icon, int dim1, int dim2)  {
+    private ImageIcon Scale(ImageIcon icon, int dim1, int dim2)  {
         // Returns an ImageIcon re-scaled to the specified size
         Image image = icon.getImage();
         Image scaled = image.getScaledInstance(dim1, dim2,  java.awt.Image.SCALE_SMOOTH);
         return new ImageIcon(scaled);
     }
 
-    public void RetrievePlayerNames()  {
+    private void RetrievePlayerNames()  {
         // Retrieve User Choice
         int noPlayers = (int)noPlayersChoice.getSelectedItem();
+        playerNames = new String[noPlayers];
         con.removeAll();
-        con.setLayout(new GridLayout((noPlayers+2),2));
+        con.setLayout(new GridLayout((noPlayers+3),1));
 
         // Player Names Prompt
         JLabel namesPrompt = new JLabel("Enter Player Names");
         namesPrompt.setFont(HEADING);
         con.add(namesPrompt);
-        con.add(new JLabel());                  // Empty Label to fill space
 
         // Player Name TextFields
-        con.add(new JLabel("Player 1:"));
-        JTextField player1Name = new JTextField();
+        player1Name = new JTextField("Player1");
         con.add(player1Name);
-        con.add(new JLabel("Player 2:"));
-        JTextField player2Name = new JTextField();
+        player2Name = new JTextField("Player2");
         con.add(player2Name);
-        con.add(new JLabel("Player 3:"));
-        JTextField player3Name = new JTextField();
+        player3Name = new JTextField("Player3");
         con.add(player3Name);
         if (noPlayers > 3)  {
-            con.add(new JLabel("Player 4:"));
-            JTextField player4Name = new JTextField();
+            player4Name = new JTextField("Player4");
             con.add(player4Name);
             if (noPlayers > 4)  {
-                con.add(new JLabel("Player 5:"));
-                JTextField player5Name = new JTextField();
+                player5Name = new JTextField("Player5");
                 con.add(player5Name);
             }
         }
@@ -227,5 +265,25 @@ public class SuperTrumpGUI extends JFrame implements ActionListener {
         invalidate();
         validate();
         repaint();
+    }
+
+    private void SavePlayerNames()  {
+        playerNames[0] = player1Name.getText();
+        playerNames[1] = player2Name.getText();
+        playerNames[2] = player3Name.getText();
+        if (playerNames.length > 3)  {
+            playerNames[3] = player4Name.getText();
+            if (playerNames.length > 4)  {
+                playerNames[4] = player5Name.getText();
+            }
+        }
+    }
+
+    private void ViewHand()  {
+        // TODO view Hand
+    }
+
+    private void TempMenu()  {
+        // TODO Temp Menu
     }
 }
